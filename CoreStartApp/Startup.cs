253d33ca2fc112d78598a09390f944d4d084fe8d
Александр Полушкин <aa.polushkin@yandex.ru>
2023-09-18 21:34:34 +0300

    Добавьте файлы проекта.

---
+++ CoreStartApp/Startup.cs
@@ -0,0 +1,96 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreStartApp
+{
+    public class Startup
+    {
+        private static IWebHostEnvironment _env;
+        // Метод вызывается средой ASP.NET.
+        // Используйте его для подключения сервисов приложения
+        // Документация:  https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // Метод вызывается средой ASP.NET.
+        // Используйте его для настройки конвейера запросов
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            _env = env;
+            if (env.IsDevelopment() || env.IsStaging())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            //Добавляем компонент для логирования запросов с использованием метода Use.
+            app.Use(async (context, next) =>
+            {
+                string fileLogger = "RequestLog.txt";
+                string path = Path.Combine(_env.ContentRootPath, "Logs", fileLogger);
+                string log = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}{Environment.NewLine}";
+
+                await File.AppendAllTextAsync(path, log);
+
+                // Для логирования данных о запросе используем свойства объекта HttpContext
+                Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
+                
+                await next.Invoke();
+            });
+
+            //Добавляем компонент с настройкой маршрутов для главной страницы
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync($"Welcome to the {env.ApplicationName}!");
+                });
+            });
+
+            // Все прочие страницы имеют отдельные обработчики
+            app.Map("/about", About);
+            app.Map("/config", Config);
+
+            // Обработчик для ошибки "страница не найдена"
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"Page not found");
+            });
+
+        }
+
+        /// <summary>
+        ///  Обработчик для страницы About
+        /// </summary>
+        private static void About(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"{_env.ApplicationName} - ASP.Net Core tutorial project");
+            });
+        }
+
+        /// <summary>
+        ///  Обработчик для страницы Config
+        /// </summary>
+        private static void Config(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"App name: {_env.ApplicationName}. App running configuration: {_env.EnvironmentName}");
+            });
+        }
+
+        
+    }
+}
